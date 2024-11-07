b5d0dfa7bcf124b979bb3cbf0f8d8429411bc9e9
DylanQL <angelo.quispe.l@tecsup.edu.pe>
2024-11-07 16:59:36 -0500

    commit

--- src/main/java/com/tecsup/petclinic/domain/VetTO.java
+++ src/main/java/com/tecsup/petclinic/domain/VetTO.java
@@ -1,4 +1,23 @@
 package com.tecsup.petclinic.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author [Your Name]
+ *
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class VetTO {
+
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
 }
